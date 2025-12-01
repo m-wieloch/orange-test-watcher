dc10b95db34c37e09a92aa92332960054b198c8a
Maciej Wieloch <macwieloch@student.agh.edu.pl>
2025-12-01 22:26:58 +0100

    test

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ from datetime import datetime, timezone
 # --- KONFIGURACJA ---
 
 # Pobieramy produkt z bezpiecznych ustawień GitHuba (Secrets)
-PRODUCT_URL = "https://www.orange.pl/esklep/smartfony/apple/iphone-17-pro-max-512gb-5g?variant=MOB_CPO_DEV_IPHONE17PM_512_5G_ES_SILVER&typ-oferty=SIMFREE&typ-procesu=SALE_OF_GOODS&id-oferty=DEFAULT_SALES_OF_GOODS_PROPOSITION$MOB_CPO_SALES_OF_GOODS&"
+# PRODUCT_URL = "https://www.orange.pl/esklep/smartfony/apple/iphone-17-pro-max-512gb-5g?variant=MOB_CPO_DEV_IPHONE17PM_512_5G_ES_SILVER&typ-oferty=SIMFREE&typ-procesu=SALE_OF_GOODS&id-oferty=DEFAULT_SALES_OF_GOODS_PROPOSITION$MOB_CPO_SALES_OF_GOODS&"
+PRODUCT_URL = "https://www.orange.pl/esklep/smartfony/samsung/samsung-galaxy-s25-5g-12-512-gb?variant=MOB_CPO_DEV_SAMS_S25_512_5G_ES_BLUE&typ-oferty=SIMFREE_INST&typ-procesu=INSTALMENT_SALES_OF_GOODS&id-oferty=TANTO_B2B_249414$MOB_CPO_7052_5722_AC_249414&market=B2B&snrai_campaign=7mV5mj3YuDjs&snrai_id=0c9bc913-841e-40bc-8cb0-f39bf18075bc&undefined"
 
 # Pobieramy webhook z bezpiecznych ustawień GitHuba (Secrets)
 DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
